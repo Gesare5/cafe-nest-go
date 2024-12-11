8c53fcd8126dddd6b8783616d0c7652a5fd115b0
Gesare5 <gesare.nyangweso@gmail.com>
2024-12-11 12:34:38 +0300

    Add: HandlePrelimAction function

--- pos-app/main.go
+++ pos-app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -24,9 +25,70 @@ func manageCoffeeItemsList() []string {
 	return manageCoffeeList
 }
 
-func handle_prelim_action(prelimAction string) {
-	println("yooooooooo!")
-	println(prelimAction)
+func manageCoffeeItems(coffeeChoice int) {
+	println("manageee!")
+	println(coffeeChoice)
+}
+
+func generate_table(lst []string) {
+	for _, value := range lst {
+		println(value)
+	}
+}
+
+func handleSelection(coffeeChoice int, quantity int) {
+	println("Handle selection")
+}
+
+func handlePrelimAction(action string) {
+	COFFEE_TABLE_SELECTION_LIST := []string{"0 Done!!", "0 Done!!"}
+	switch action {
+	case "a":
+		COFFEE_TABLE_SELECTION_LIST = append(COFFEE_TABLE_SELECTION_LIST, "3 Done!!")
+		looping := true
+		for looping == true {
+			print("")
+			generate_table(COFFEE_TABLE_SELECTION_LIST)
+			coffeeChoice := ""
+			go fmt.Scanf("%d", &coffeeChoice)
+			coffeeChoiceInt, err := strconv.Atoi(coffeeChoice)
+			if err != nil {
+				coffeeChoiceInt = 0
+			}
+
+			if coffeeChoiceInt < len(COFFEE_TABLE_SELECTION_LIST) {
+				if coffeeChoiceInt == 0 {
+					break
+				} else {
+					print("How many? ")
+					quantity := ""
+					fmt.Scanf("%d", &quantity)
+					quantityInt, err := strconv.Atoi(quantity)
+					if err == nil {
+						handleSelection(coffeeChoiceInt, quantityInt)
+						print("")
+					} else {
+						print("Invalid value!!")
+						print("")
+						break
+					}
+				}
+			} else {
+				print("Invalid Choice!!")
+				continue
+			}
+		}
+	case "b":
+		print("")
+		generate_table(manageCoffeeItemsList())
+		coffeeItemsChoice := 0
+		fmt.Scanf("%d", &coffeeItemsChoice)
+		manageCoffeeItems(coffeeItemsChoice)
+		print("")
+
+	default:
+		return
+	}
 }
 
 func main() {
@@ -34,8 +96,8 @@ func main() {
 	fmt.Print("Date", time.Now())
 	println("Welcome to TeleBaristas!")
 	println("")
-	keep_looping := true
-	for keep_looping == true {
+	keepLooping := true
+	for keepLooping == true {
 		for _, value := range prelimList() {
 			println(value)
 		}
@@ -45,7 +107,7 @@ func main() {
 		if prelimAction == "c" {
 			break
 		} else {
-			handle_prelim_action(prelimAction)
+			handlePrelimAction(prelimAction)
 		}
 	}
 }
