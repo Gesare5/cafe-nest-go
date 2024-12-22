e37dabea943ebab6f98d8d239888c258abecb06e
Gesare5 <gesare.nyangweso@gmail.com>
2024-12-22 20:40:19 +0300

    Add: Initial setup for datastore operations

---
+++ pos-app/data_store_operations.go
@@ -0,0 +1,14 @@
+package main
+
+func readFromStore(filename string) []string {
+	var readList []string
+	return readList
+}
+
+func writeToStore(filename string, data []string) error {
+	return nil
+}
+
+func overwriteStore(filename string, data []string) error {
+	return nil
+}
