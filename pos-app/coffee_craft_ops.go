6dc54e963616b5b7cfda97466361412962f235ed
Gesare5 <gesare.nyangweso@gmail.com>
2024-12-27 16:00:59 +0300

    Change: Update read_from_store function with readfile method Add: Start setup for coffee_craft_ops.go Add: Sample csv file to test read_from_store function

---
+++ pos-app/coffee_craft_ops.go
@@ -0,0 +1,17 @@
+package main
+
+func get_totals() {
+	return
+}
+
+func getThresholds() {
+	return
+}
+
+func populateCoffeeItems() {
+	return
+}
+
+func generateCoffeeList() {
+	return
+}
--- pos-app/data_store_operations.go
+++ pos-app/data_store_operations.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func readFromStore(filename string) []string {
+	// READ FROM FILE
+	data, err := os.ReadFile(filename)
+	check(err)
+	fmt.Print(string(data))
+
 	var readList []string
 	return readList
 }
