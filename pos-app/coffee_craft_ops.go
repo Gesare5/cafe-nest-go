0b8a2527da15ba0803d792359e11bb16249c8a9b
Gesare5 <gesare.nyangweso@gmail.com>
2024-12-30 15:52:36 +0300

    Change: Create temporary static maps for thresholds and totals

--- pos-app/coffee_craft_ops.go
+++ pos-app/coffee_craft_ops.go
@@ -1,12 +1,28 @@
 package main
 
-func get_totals() {
-	return
+func get_totals() map[string]float32 {
+	totals := map[string]float32{
+		"coffee":  10000,
+		"milk":    10000,
+		"sugar":   10000,
+		"vanilla": 1000,
+		"cocoa":   200,
+	}
+	return totals
 }
 
-func getThresholds() {
-	return
+func getThresholds() map[string]float32 {
+	thresholds := map[string]float32{
+		"coffee":  10000,
+		"milk":    10000,
+		"sugar":   10000,
+		"vanilla": 1000,
+		"cocoa":   200,
+	}
+	return thresholds
 }
+
+// TODO: Read above values from files - later
 
 func populateCoffeeItems() {
 	return
