c9e9c04845228e82a058ac593d69e59c6b933cad
Gesare5 <gesare.nyangweso@gmail.com>
2024-12-31 13:03:46 +0300

    Change: create the writeToStore and overwriteStore operations

--- pos-app/data_store_operations.go
+++ pos-app/data_store_operations.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func check(e error) {
@@ -15,16 +16,39 @@ func readFromStore(filename string) []string {
 	// READ FROM FILE
 	data, err := os.ReadFile(filename)
 	check(err)
-	fmt.Print(string(data))
-
-	var readList []string
-	return readList
+	dataList := strings.Split(string(data), "\n")
+	return dataList
 }
 
-func writeToStore(filename string, data []string) error {
+func writeToStore(filename string, data string) {
+	// ADD/APPEND DATA TO STORE
+	oldData, err := os.ReadFile(filename)
+	check(err)
+	dataString := string(oldData)
+
+	fmt.Println("Writing file")
+	file, err := os.Create(filename)
+	check(err)
+
+	_, err = file.WriteString(dataString + data + "\n")
+	check(err)
+	fmt.Printf("File name: %s \n", file.Name())
+}
+
+func overwriteStore(filename string, data string) error {
+	// POPULATE DATA STORE WITH ALL NEW DATA
+	fmt.Println("Writing file")
+	file, err := os.Create(filename)
+	check(err)
+
+	_, err = file.WriteString(data + "\n")
+	check(err)
+	fmt.Printf("File name: %s \n", file.Name())
 	return nil
 }
 
-func overwriteStore(filename string, data []string) error {
-	return nil
-}
+// func main() {
+// 	data := "blue,150,18,30,0,0,12"
+// 	writeToStore("coffee_items.csv", data)
+// 	overwriteStore("coffee_items.csv", data)
+// }
